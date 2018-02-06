8227bb8baaf439c1642bca7bef29b5db2b842664
Rommel Rico <rommeltj@gmail.com>
2018-02-05 20:16:18 -0800

    Search by Related Model

--- src/products/models.py
+++ src/products/models.py
@@ -32,7 +32,9 @@ class ProductQuerySet(models.QuerySet):
     def search(self, query):
         lookups = (Q(title__icontains=query) |
                    Q(description__icontains=query) |
-                   Q(price__icontains=query))
+                   Q(price__icontains=query) |
+                   Q(tag__title__icontains=query)
+                   )
         return self.filter(lookups).distinct()
 
 
