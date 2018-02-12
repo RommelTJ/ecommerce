79e2ebde2a9ead1463c9c9957bd20268ad0111b8
Rommel Rico <rommeltj@gmail.com>
2018-02-11 19:15:40 -0800

    Generate the Order ID

--- src/orders/models.py
+++ src/orders/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.db.models.signals import pre_save
 
 from carts.models import Cart
+from ecommerce.utils import unique_order_id_generator
+
 
 
 ORDER_STATUS_CHOICES = (
@@ -12,6 +15,7 @@ ORDER_STATUS_CHOICES = (
 
 
 # Create your models here.
+# Random, Unique
 class Order(models.Model):
     order_id = models.CharField(max_length=120, blank=True)  # AB31DE3
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
@@ -21,3 +25,11 @@ class Order(models.Model):
 
     def __str__(self):
         return self.order_id
+
+
+def pre_save_create_order_id(sender, instance, *args, **kwargs):
+    if not instance.order_id:
+        instance.order_id = unique_order_id_generator(instance)
+
+
+pre_save.connect(pre_save_create_order_id, sender=Order)
