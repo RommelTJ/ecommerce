51b6d2738dd5da957aab30c3d9b86e2a7a64d29c
Rommel Rico <me@rommelrico.com>
2018-03-01 08:13:27 -0800

    Add Card to Customer with Stripe

--- src/billing/views.py
+++ src/billing/views.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.conf import settings
 from django.utils.http import is_safe_url
 
@@ -7,9 +7,14 @@ import stripe
 stripe.api_key = settings.STRIPE_API_KEY
 STRIPE_PUB_KEY = settings.STRIPE_PUB_KEY
 
+from .models import BillingProfile
+
 
 # Create your views here.
 def payment_method_view(request):
+    billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+    if not billing_profile:
+        return redirect("/cart")
     next_url = None
     next_ = request.GET.get('next')
     if is_safe_url(next_, request.get_host()):
@@ -19,5 +24,12 @@ def payment_method_view(request):
 
 def payment_method_createview(request):
     if request.method == "POST" and request.is_ajax():
+        billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+        if not billing_profile:
+            return HttpResponse({"message": "Cannot find this user"}, status_code=401)
+        token = request.POST.get("token")
+        if token is not None:
+            customer = stripe.Customer.retrieve(billing_profile.customer_id)
+            card_response = customer.sources.create(source=token)
         return JsonResponse({"message": "Success! Your card was added."})
     return HttpResponse("error", status_code=401)
