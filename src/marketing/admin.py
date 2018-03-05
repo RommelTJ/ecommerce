a78c32aaee08ce7f4a4eeb33a04ca441a20c50bf
Rommel Rico <rommeltj@gmail.com>
2018-03-04 16:32:44 -0800

    Fixing a typo and adding marketing preferences to the Django admin

--- src/marketing/admin.py
+++ src/marketing/admin.py
@@ -4,3 +4,20 @@ from .models import MarketingPreference
 
 
 # Register your models here.
+class MarketingPreferenceAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'subscribed', 'updated']
+    readonly_fields = ['mailchimp_msg', 'mailchimp_subscribed', 'timestamp', 'updated']
+
+    class Meta:
+        model = MarketingPreference
+        fields = [
+            'user',
+            'subscribed',
+            'mailchimp_msg',
+            'mailchimp_subscribed',
+            'timestamp',
+            'updated'
+        ]
+
+
+admin.site.register(MarketingPreference, MarketingPreferenceAdmin)
