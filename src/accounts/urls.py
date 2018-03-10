9e35aed792b44064a307bfe83af1960a91446852
Rommel Rico <rommeltj@gmail.com>
2018-03-10 13:36:17 -0800

    Email Reactivation

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -7,4 +7,5 @@ app_name = 'accounts'
 urlpatterns = [
     url(r'^$', AccountHomeView.as_view(), name='home'),
     url(r'^email/confirm/(?P<key>[0-9A-Za-z]+)/$', AccountEmailActivateView.as_view(), name='email-activate'),
+    url(r'^email/resend-activation/$', AccountEmailActivateView.as_view(), name='resend-activation'),
 ]
