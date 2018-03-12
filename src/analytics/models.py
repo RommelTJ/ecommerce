a6120407b167f910d2724d9a7e383c40578e8e99
Rommel Rico <me@rommelrico.com>
2018-03-12 08:07:39 -0700

    User Product History View

--- src/analytics/models.py
+++ src/analytics/models.py
@@ -15,6 +15,26 @@ FORCE_INACTIVE_USER_ENDSESSION= getattr(settings, 'FORCE_INACTIVE_USER_ENDSESSIO
 
 
 # Create your models here.
+class ObjectViewedQuerySet(models.query.QuerySet):
+
+    def by_model(self, model_class, model_queryset=False):
+        c_type = ContentType.objects.get_for_model(model_class)
+        qs = self.filter(content_type=c_type)
+        if model_queryset:
+            viewed_ids = [x.object_id for x in qs]
+            return model_class.objects.filter(pk__in=viewed_ids)
+        return qs
+
+
+class ObjectViewedManager(models.Manager):
+
+    def get_queryset(self):
+        return ObjectViewedQuerySet(self.model, using=self._db)
+
+    def by_model(self, model_class, model_queryset=False):
+        return self.get_queryset().by_model(model_class, model_queryset=model_queryset)
+
+
 class ObjectViewed(models.Model):
     user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)  # User instance instance.id
     ip_address = models.CharField(max_length=220, blank=True, null=True)  # IP Field
@@ -22,6 +42,8 @@ class ObjectViewed(models.Model):
     object_id = models.PositiveIntegerField()  # User id, Product id, Order id,
     content_object = GenericForeignKey('content_type', 'object_id')  # Product instance
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    objects = ObjectViewedManager()
 
     def __str__(self):
         return "%s viewed on %s" %(self.content_object, self.timestamp)
