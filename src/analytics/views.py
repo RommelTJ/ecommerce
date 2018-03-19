6d5afdef2419da63f33eb54c8512d0e6b6df5f9c
Rommel Rico <me@rommelrico.com>
2018-03-19 08:28:41 -0700

    Setup the Sales View

--- src/analytics/views.py
+++ src/analytics/views.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
+from django.views.generic import TemplateView
 from django.shortcuts import render
 
+
 # Create your views here.
+class SalesView(LoginRequiredMixin, TemplateView):
+    template_name = "analytics/sales.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        user = self.request.user
+        if not user.is_staff:
+            return render(self.request, "400.html", {})
+        return super(SalesView, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        return super(SalesView, self).get_context_data(**kwargs)
