e8c467080b2edf1dd2bc550f262116115b7053b1
Rommel Rico <rommeltj@gmail.com>
2018-03-21 13:16:02 -0700

    Aggregate and Annotate

--- src/analytics/views.py
+++ src/analytics/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Count, Sum, Avg
 from django.views.generic import TemplateView
 from django.shortcuts import render
 
@@ -20,10 +21,10 @@ class SalesView(LoginRequiredMixin, TemplateView):
         qs = Order.objects.all()
         context['orders'] = qs
         context['recent_orders'] = qs.recent().not_refunded()[:5]
-        recent_orders_total = 0
-        for i in context['recent_orders']:
-            recent_orders_total += i.total
-        context['recent_orders_total'] = recent_orders_total
+        context['recent_orders_total'] = context['recent_orders'].aggregate(
+            Sum("total"),
+            Avg("total"),
+        )
         context['shipped_orders'] = qs.recent().not_refunded().by_status(status='shipped')[:5]
         context['paid_orders'] = qs.recent().not_refunded().by_status(status='paid')[:5]
         return context
