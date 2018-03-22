c1242738a074c32d38ebcdd450caa7f52db68619
Rommel Rico <me@rommelrico.com>
2018-03-22 08:35:46 -0700

    Rendering actual sales data using ChartJS and Ajax

--- src/analytics/views.py
+++ src/analytics/views.py
@@ -1,3 +1,5 @@
+import random
+import datetime
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import JsonResponse
 from django.views.generic import TemplateView, View
@@ -13,12 +15,31 @@ class SalesAjaxView(View):
     def get(self, request, *args, **kwargs):
         data = {}
         if request.user.is_staff:
+            qs = Order.objects.all().by_weeks_range(weeks_ago=5, number_of_weeks=5)
             if request.GET.get('type') == 'week':
-                data['labels'] = ['Mon', 'Tues', 'Weds', 'Thurs', 'Fri', 'Sat', 'Sun']
-                data['data'] = [123, 131, 232, 12, 323, 313, 3193]
+                days = 7
+                start_date = timezone.now().today() - datetime.timedelta(days=days-1)
+                datetime_list = []
+                labels = []
+                sales_items = []
+                for x in range(0, days):
+                    new_time = start_date + datetime.timedelta(days=x)
+                    datetime_list.append(new_time)
+                    labels.append(new_time.strftime("%a"))
+                    new_qs = qs.filter(updated__day=new_time.day, updated__month=new_time.month)
+                    day_total = new_qs.totals_data()['total__sum'] or 0
+                    sales_items.append(day_total)
+                data['labels'] = labels
+                data['data'] = sales_items
             if request.GET.get('type') == '4weeks':
-                data['labels'] = ['Last Week', 'Two Weeks Ago', 'Three Weeks Ago', 'Four Weeks Ago']
-                data['data'] = [123, 131, 323, 3193]
+                data['labels'] = ["Four Weeks Ago", "Three Weeks Ago", "Two Weeks Ago", "Last Week", "This Week"]
+                current = 5
+                data['data'] = []
+                for i in range(0, 5):
+                    new_qs = qs.by_weeks_range(weeks_ago=current, number_of_weeks=1)
+                    sales_total = new_qs.totals_data()['total__sum'] or 0
+                    data['data'].append(sales_total)
+                    current -= 1
         return JsonResponse(data)
 
 
